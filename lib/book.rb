bfe0c22e89337bc104f4ee1d88a8c5a139ec5c36
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-27 17:14:39 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,11 @@
 # book.rb
 class Book
+attr_reader :title
+attr_accessor :author, :page_count, :genre
+
 def initialize(title)
    
-# @title = title
+@title = title
 #   end 
 # def title 
 #   @title
@@ -14,18 +17,18 @@ def author
   @author
 end 
 
-def page_count= (page_count)
-  @page_count = page_count
-  end 
-  def page_count
-    @page_count
-  end 
-  def genre= (genre)
- @genre = genre
- end 
- def genre
- @genre
-  end 
+# def page_count= (page_count)
+#   @page_count = page_count
+#   end 
+#   def page_count
+#     @page_count
+#   end 
+#   def genre= (genre)
+# @genre = genre
+# end 
+# def genre
+# @genre
+#   end 
 def turn_page
 puts "Flipping the page...wow, you read fast!"
   end 
