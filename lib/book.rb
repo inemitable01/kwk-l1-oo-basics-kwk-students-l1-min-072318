129583698d58b01b0fef50d4692b53b681d2891a
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-27 17:14:50 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,17 +5,17 @@ attr_accessor :author, :page_count, :genre
 
 def initialize(title)
    
-@title = title
+# @title = title
+# #   end 
+# # def title 
+# #   @title
+# #   end
+# def author= (author)
+#   @author= author
 #   end 
-# def title 
-#   @title
-#   end
-def author= (author)
-  @author= author
-  end 
-def author
-  @author
-end 
+# def author
+#   @author
+# end 
 
 # def page_count= (page_count)
 #   @page_count = page_count
