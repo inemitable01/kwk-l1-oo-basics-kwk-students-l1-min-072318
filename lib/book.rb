985b5a150ce786842b1895c27f733ddb87c60875
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-27 17:11:12 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -20,11 +20,14 @@ def page_count= (page_count)
   def page_count
     @page_count
   end 
-  def genre = (genre)
+  def genre= (genre)
  @genre = genre
  end 
  def genre
  @genre
+end 
+def turn_page
+puts "Flipping the page...wow, you read fast!"
 end 
 end 
 book = Book.new ("And Then There Were None")
