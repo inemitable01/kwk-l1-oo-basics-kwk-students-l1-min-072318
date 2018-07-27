632ee0ecda0ad76121b2ccc82eb86001e9ae7dee
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-27 17:16:22 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,7 @@
 # book.rb
 class Book
-attr_reader :title
-attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
 
   def initialize(title)
     @title = title
