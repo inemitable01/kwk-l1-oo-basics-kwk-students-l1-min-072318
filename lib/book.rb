2b3acd7b27b64c2c9026f0383f270d9adf77e928
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-27 17:16:38 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,36 @@
 # book.rb
-# Add you Book class here
+class Book
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
+
+  def initialize(title)
+    @title = title
+  end 
+# # def title 
+# #   @title
+# #   end
+# def author= (author)
+#   @author= author
+#   end 
+# def author
+#   @author
+# end 
+
+# def page_count= (page_count)
+#   @page_count = page_count
+#   end 
+#   def page_count
+#     @page_count
+#   end 
+#   def genre= (genre)
+# @genre = genre
+# end 
+# def genre
+# @genre
+#   end 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+end 
+book = Book.new ("And Then There Were None")
+
